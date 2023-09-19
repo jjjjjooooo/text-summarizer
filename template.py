7016f09fd85d865bafe4e62c60d1d8ebe6447291
Joe Jin <jinoujoe@gmail.com>
2023-09-20 00:51:08 +0200

    pass removed

--- template.py
+++ template.py
@@ -40,7 +40,6 @@ for filepath in list_of_files:
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass
             logging.info(f"Creating empty file: {filepath}")
 
     else:
